f2de8fc1bb47b353018f4e5178be43c16afaf574
maiaboelmagd <maishalan00000@gmail.com>
2025-06-11 02:36:53 +0300

    feat(theme): add light and dark theme data, controller, and SharedPreferences integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
+import 'package:cakes_store_frontend/core/components/navigation_bar.dart';
+import 'package:cakes_store_frontend/core/services/preference_manager.dart';
+import 'package:cakes_store_frontend/core/theme/dark_theme.dart';
+import 'package:cakes_store_frontend/core/theme/light_theme.dart';
+import 'package:cakes_store_frontend/core/theme/theme_controller.dart';
+import 'package:cakes_store_frontend/test_screens/theme_showcase_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'core/components/navigation_bar.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferencesManager().init();
+  await ThemeController().init();
 
-void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-
-      home: NavigationBarScreen(),
+    return ValueListenableBuilder(
+      valueListenable: ThemeController.themeNotifier,
+      builder: (_, value, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'YumSlice',
+          themeMode: value,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          home: NavigationBarScreen(),
+          //home: ThemeShowcaseScreen(),
+        );
+      },
     );
   }
 }
