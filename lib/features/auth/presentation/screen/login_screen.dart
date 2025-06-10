c7066e6544f7a09088632cb40500c60a35e45c25
Monica Saeed <monicasaeed12@gmail.com>
2025-06-11 00:59:54 +0300

    login screen

--- lib/features/auth/presentation/screen/login_screen.dart
+++ lib/features/auth/presentation/screen/login_screen.dart
@@ -1,24 +1,54 @@
+import 'package:cakes_store_frontend/features/auth/presentation/screen/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../business/auth_cubit.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Icon(Icons.login, size: 64, color: Colors.blue),
-            const SizedBox(height: 16),
-            const Text(
-              'Welcome Back!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
-            const SizedBox(height: 8),
-            const Text('Please log in to continue.'),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                context.read<AuthCubit>().loginUser(
+                  emailController.text.trim(),
+                  passwordController.text.trim(),
+                );
+              },
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegisterScreen(),
+                  ),
+                );
+              },
+              child: const Text('Register'),
+            ),
           ],
         ),
       ),
