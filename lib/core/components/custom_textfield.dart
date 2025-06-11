638b5c9f8eea00bd48aed1d440db4fd5f87a5b9f
SaraElsayed <esara7772@gmail.com>
2025-06-11 09:02:18 +0300

    "addvalidator"

--- lib/core/components/custom_textfield.dart
+++ lib/core/components/custom_textfield.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
- CustomTextfield({super.key,required this.title, this.hintText,required this.controller, this.obscureText = false});
+ CustomTextfield({super.key,required this.title, this.hintText,required this.controller, this.obscureText = false,required this.validator});
 
   String title;
   String? hintText;
   TextEditingController? controller;
   bool obscureText;
+ final String? Function(String?) validator;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             title,
@@ -27,10 +30,13 @@ class CustomTextfield extends StatelessWidget {
             obscureText: obscureText,
             decoration: InputDecoration(
               hintText: hintText,
+              fillColor: Colors.white,
+              filled: true,
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(5),
               ),
             ),
+            validator: validator,
           ),
         ],
       ),
