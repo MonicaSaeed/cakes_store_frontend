0bfca75078dede46edba47076b31e4cf975e8bf5
HebaBakry <Heba.m.bakry@gmail.com>
2025-06-13 16:22:45 +0200

    make_api_constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,11 @@
+class ApiConstance {
+  static const String baseUrl = "http://10.0.2.2:1000";
+  static const String productsUrl = "$baseUrl/products";
+  static const String usersUrl = "$baseUrl/users";
+  static const String promocodesUrl = "$baseUrl/promocodes";
+  static const String ordersUrl = "$baseUrl/orders";
+  static const String payUrl = "$baseUrl/pay";
+  static const String favsUrl = "$baseUrl/favs";
+  static const String cartUrl = "$baseUrl/cart";
+  static const String reviewsUrl = "$baseUrl/reviews";
+}
