0c69bcdd04018c29d8309b7889ab7a2f25713258
Yasmeen Alaa <y.alaa9.2001@gmail.com>
2025-06-13 19:22:44 +0300

    make_service_locator

--- lib/features/shop/presentation/cubit/product_list_cubit.dart
+++ lib/features/shop/presentation/cubit/product_list_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:cakes_store_frontend/core/services/service_locator.dart';
 import 'package:cakes_store_frontend/features/shared_product/data/models/product_model.dart';
 import 'package:cakes_store_frontend/features/shop/data/datasource/product_data_source.dart';
 import 'package:cakes_store_frontend/features/shop/data/repository/products_repository.dart';
@@ -9,12 +10,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class ProductListCubit extends Cubit<ProductListState> {
   ProductListCubit() : super(const ProductListLoading());
 
-  ProductDataSource _dataSource = ProductDataSource();
+  // ProductDataSource _dataSource = ProductDataSource();
   getProductList() async {
     try {
-      final BaseProductsRepository _repo = ProductsRepository(_dataSource);
+      // final BaseProductsRepository _repo = ProductsRepository(_dataSource);
       List<ProductModel>? products =
-          await GetProductListUseCase(_repo).getProductList();
+          await GetProductListUseCase(
+            sl<BaseProductsRepository>(),
+          ).getProductList();
+
       if (products != null) {
         print('from cubit $products');
         emit(ProductListLoaded(products));
