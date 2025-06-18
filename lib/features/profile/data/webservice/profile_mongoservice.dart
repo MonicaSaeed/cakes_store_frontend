c96b40f825c1d7767f0e0b3331c976aae120921d
SaraElsayed <esara7772@gmail.com>
2025-06-19 00:25:45 +0300

    "editprofileservice"

--- lib/features/profile/data/webservice/profile_mongoservice.dart
+++ lib/features/profile/data/webservice/profile_mongoservice.dart
@@ -6,10 +6,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 
 class ProfileService {
-
-  User? currentuser;
-  List<ProfileModel>? allusers;
-
   
   ProfileService._internal();
   static final ProfileService _instance = ProfileService._internal();
@@ -17,52 +13,87 @@ class ProfileService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
 
-    Future<User?> getCurrentUser() async {
+  Future<User?> getCurrentUser() async {
     return _auth.currentUser;
   }
 
 
-Future<ProfileModel?> getProfileFromMongo() async {
-  
+  Future<ProfileModel?> getProfileFromMongo() async {
+    try {
+      User? _user = await getCurrentUser();
+      String? uid = _user?.uid;
+
+      print("Current User ID: $uid");
+
+      final response = await http.get(Uri.parse('http://192.168.126.44:1000/users/uid/$uid'));
+
+      print("Response: ${response.body}");
+
+      if (response.statusCode == 200)
+       {
+        
+        // final Map<String, dynamic> data = json.decode(response.body);
+        // print("Decoded Data: $data");
+        // return ProfileModel.fromJson(data);
+
+final Map<String, dynamic> responseData = json.decode(response.body);
+final Map<String, dynamic> userData = responseData['data'];
+print("Decoded Data: $userData");
+return ProfileModel.fromJson(userData); 
+
+      } 
+      else
+       {
+        throw Exception('Failed to fetch profile');
+      }
+    } catch (e) {
+      print('Profile fetch error: $e');
+      rethrow;
+    }
+  }
+
+ Future<void> UpdateProfile(ProfileModel profile) async {
+
   String? uid;
   User? _user = await getCurrentUser();
   uid = _user?.uid;
 
- try
- { 
-  final response = await http.get(Uri.parse('http://localhost:1000/users/$uid'));
-
-   if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    return ProfileModel.fromJson(data);
-  } 
-  else 
-  {
-    throw Exception("Profile not found");
-  }
-  }catch (e) {
-    print("Error fetching profile: $e");
-    return null;
-  }
-
-}
-
- Future<void> UpdateProfile(ProfileModel profile) async {
-
   try
   {
-   final response = await http.put(Uri.parse("http://localhost:1000/users/uid/${profile.uid}"),
+   final response = await http.put(Uri.parse("http://192.168.126.44:1000/users/uid/$uid"),
       headers: {"Content-Type": "application/json"},
       body: json.encode(profile.toJson()),
     );
 
     if (response.statusCode != 200) {
-      throw Exception("Failed to update profile");
+      throw Exception("Failed to update profile"+ " ${response.statusCode}");
     }}
     catch (e) {
       print("Error updating profile: $e");
       throw e;
     }
   }
+
+//  Future<void> getOrdersByUserId(String userId) async {
+
+//   String? userid;
+//     User? _user = await getCurrentUser();
+//     userid = _user?.id;
+
+
+//     try{
+//       final response = await http.get(Uri.parse('http://http://192.168.126.44:1000/user/$userId'));
+//       }
+//       catch (e) {
+//         print('Error fetching orders: $e');
+//         throw e;
+
+//       }
+//      }
+
+  
+
+
+
 
 }
