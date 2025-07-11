3b47454a924ff4d3c14b5e69bbfc57265c435a22
maiaboelmagd <maishalan00000@gmail.com>
2025-07-12 02:09:56 +0300

    implement full payment flow in order screen

--- lib/features/orders/presentation/screen/components/status_components.dart
+++ lib/features/orders/presentation/screen/components/status_components.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
+import 'package:cakes_store_frontend/core/constants/api_constants.dart';
 import 'package:cakes_store_frontend/features/orders/domain/entities/order_entity.dart';
 import 'package:cakes_store_frontend/features/orders/presentation/cubit/orders_cubit/get_orders_cubit.dart';
 import 'package:cakes_store_frontend/features/orders/presentation/screen/components/info_row.dart';
+import 'package:cakes_store_frontend/features/payment/payment_screen.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -15,44 +20,102 @@ class StatusComponent extends StatefulWidget {
 
 class _StatusComponentState extends State<StatusComponent> {
   late String _status;
+  late String _paymentStatus;
 
   @override
   void initState() {
     _status = widget.order.orderStatus;
+    _paymentStatus = widget.order.paymentStatus;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return 
-       Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (_status == 'pending')
-            SizedBox(
-              width: 300,
-              child: ElevatedButton(
-                onPressed: () async {
-                  await GetOrdersCubit().cancelOrder(widget.order.id);
-                  setState(() {
-                    _status = 'Canceled';
-                  });
-                },
-                child: const Text('Cancel'),
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (_paymentStatus == 'onDelivery' &&
+            _status != 'delivered' &&
+            _status != 'Canceled')
+          SizedBox(
+            width: 300,
+            child: ElevatedButton(
+              onPressed: () async {
+                try {
+                  // Step 1: Get iframe URL from your backend
+                  final response = await Dio().post(
+                    ApiConstance.payUrl,
+                    data: {
+                      "mongo_order_id": widget.order.id,
+                      "amount": widget.order.totalPrice,
+                    },
+                  );
+
+                  if (response.statusCode == 200 &&
+                      response.data['iframe_url'] != null) {
+                    final iframeUrl = response.data['iframe_url'];
+
+                    // Step 2: Open PaymentScreen and wait for result
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => PaymentScreen(iframeUrl: iframeUrl),
+                      ),
+                    );
+                  }
+                  // Fetch updated order data
+                  final statusResponse = await Dio().get(
+                    '${ApiConstance.ordersUrl}/${widget.order.id}',
+                  );
+
+                  if (statusResponse.statusCode == 200 &&
+                      statusResponse.data['success'] == true) {
+                    final orderData = statusResponse.data['data'];
+
+                    setState(() {
+                      _status = orderData['orderStatus'];
+                      _paymentStatus = orderData['paymentStatus'];
+                    });
+                  } else {
+                    log(
+                      "Order fetch failed (StatusComponent widget): ${response.data['message']}",
+                    );
+                  }
+                } catch (e) {
+                  log(
+                    "Error while fetching order status(StatusComponent widget): $e",
+                  );
+                }
+              },
+
+              child: const Text('Pay'),
             ),
-      
-          InfoRow(label: 'Order Status:', value: _status),
-          InfoRow(label: 'Payment Status:', value: widget.order.paymentStatus),
-          InfoRow(
-            label: 'Created At:',
-            value:
-                widget.order.createdAt != null
-                    ? DateFormat.yMMMMd().add_jm().format(widget.order.createdAt!)
-                    : 'N/A',
           ),
-        ],
-      
+        if (_status == 'pending')
+          SizedBox(
+            width: 300,
+            child: ElevatedButton(
+              onPressed: () async {
+                await GetOrdersCubit().cancelOrder(widget.order.id);
+                setState(() {
+                  _status = 'Canceled';
+                });
+              },
+              child: const Text('Cancel'),
+            ),
+          ),
+
+        InfoRow(label: 'Order Status:', value: _status),
+        InfoRow(label: 'Payment Status:', value: _paymentStatus),
+        InfoRow(
+          label: 'Created At:',
+          value:
+              widget.order.createdAt != null
+                  ? DateFormat.yMMMMd().add_jm().format(widget.order.createdAt!)
+                  : 'N/A',
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await PreferencesManager().init();
   await ThemeController().init();
