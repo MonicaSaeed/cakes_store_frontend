fa9326db5cb14f1982e49608e232f0c7c2b5349d
maiaboelmagd <maishalan00000@gmail.com>
2025-07-12 18:04:11 +0300

    enable payment flow by navigating to PaymentScreen after checkout

--- lib/features/check_out/data/repo/order_repo_base.dart
+++ lib/features/check_out/data/repo/order_repo_base.dart
@@ -1,6 +1,7 @@
 import 'package:cakes_store_frontend/features/check_out/data/models/order_model.dart';
+import 'package:cakes_store_frontend/features/orders/domain/entities/order_entity.dart';
 
 abstract class OrderRepositoryBase {
-  Future<void> placeOrder(Order order);
+  Future<OrderEntity> placeOrder(Order order);
   Future<void> clearCart(String userId);
 }
