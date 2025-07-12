ff60b8eab3843fb50422e275944396c275787682
maiaboelmagd <maishalan00000@gmail.com>
2025-07-12 20:12:11 +0300

    show CircularProgressIndicator while payment form isLoading

--- lib/features/payment/payment_screen.dart
+++ lib/features/payment/payment_screen.dart
@@ -12,21 +12,35 @@ class PaymentScreen extends StatefulWidget {
 
 class _PaymentScreenState extends State<PaymentScreen> {
   late final WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.iframeUrl));
+    _controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onPageFinished: (url) {
+                setState(() {
+                  _isLoading = false;
+                });
+              },
+            ),
+          )
+          ..loadRequest(Uri.parse(widget.iframeUrl));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Complete Payment")),
-      body: WebViewWidget(controller: _controller),
+      body:
+          _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : WebViewWidget(controller: _controller),
     );
   }
 }
