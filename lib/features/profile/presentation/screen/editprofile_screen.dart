15614004baa4728307fd642b4063d4986f93a488
SaraElsayed <esara7772@gmail.com>
2025-07-12 16:36:13 +0300

    "editdesignofaddresses"

--- lib/features/profile/presentation/screen/editprofile_screen.dart
+++ lib/features/profile/presentation/screen/editprofile_screen.dart
@@ -2,6 +2,7 @@ import 'package:cakes_store_frontend/core/components/custom_elevated_button.dart
 import 'package:cakes_store_frontend/core/components/custom_text_field.dart';
 import 'package:cakes_store_frontend/core/extensions/extensions.dart';
 import 'package:cakes_store_frontend/core/services/toast_helper.dart';
+import 'package:cakes_store_frontend/core/theme/theme_controller.dart';
 import 'package:cakes_store_frontend/features/profile/business/cubit/user_profile_cubit.dart';
 import 'package:cakes_store_frontend/features/profile/data/model/profile_mongo_model.dart';
 import 'package:cakes_store_frontend/features/profile/presentation/widget/addaddress.dart';
@@ -127,78 +128,106 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 }
               }
 
-              return Padding(
-                padding: const EdgeInsets.all(16),
-                child: Form(
-                  key: _formKey,
-                  child: ListView(
-                    children: [
-                      const SizedBox(height: 10),
-                      Text(
-                        'Email',
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: _originalProfile!.email ?? '',
-                        ),
-                        enabled: false,
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelLarge
-                            ?.copyWith(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onSurface
-                                    .withOpacity(0.6)),
-                      ),
-                      const SizedBox(height: 16),
-                      CustomTextField(
-                        title: 'Username',
-                        hintText: _originalProfile!.username ?? '',
-                        controller: _usernameController,
-                      ),
-                      const SizedBox(height: 16),
-                      CustomTextField(
-                        title: 'Phone Number',
-                        hintText: _originalProfile!.phoneNumber ?? '',
-                        controller: _phoneController,
-                      ),
-                       const SizedBox(height: 16),
-                      ..._addressControllers.asMap().entries.map((entry) {
-                        int index = entry.key;
-                        TextEditingController controller = entry.value;
-                        return Row(
-                          children: [
-                            Expanded(
-                              child: CustomTextField(
-                                title: 'Address ${index + 1}',
-                                hintText: 'Enter your address',
-                                controller: controller,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 20),
-                              child: IconButton(
-                                icon: const Icon(Icons.delete),
-                                onPressed: () => _removeAddressField(index),
-                              ),
-                            ),
-                          ],
-                        );
-                      }).toList(),
-                
-                    addaddresas(context, _addAddressField),
-
-                      const SizedBox(height: 70),
-                      CustomElevatedButton(
-                        textdata: "Save",
-                        onPressed: _saveProfile,
-                      ),
-                    ],
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    'Email',
+                    style: Theme.of(context).textTheme.labelLarge,
                   ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: _originalProfile!.email ?? '',
+                    ),
+                    enabled: false,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge
+                        ?.copyWith(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSurface
+                                .withOpacity(0.6)),
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextField(
+                    title: 'Username',
+                    hintText: _originalProfile!.username ?? '',
+                    controller: _usernameController,
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextField(
+                    title: 'Phone Number',
+                    hintText: _originalProfile!.phoneNumber ?? '',
+                    controller: _phoneController,
+                  ),
+                    const SizedBox(height: 16),
+              Container(
+              decoration: BoxDecoration(
+                color: ThemeController.isDark()?Colors.black :Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 6,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Addresses",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 8),
+              ..._addressControllers.asMap().entries.map((entry) {
+                int index = entry.key;
+                TextEditingController controller = entry.value;
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomTextField(
+                          title: 'Address ${index + 1}',
+                          hintText: 'Enter your address',
+                          controller: controller,
+                        ),
+                      ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () => _removeAddressField(index),
+                      ),
+                    ),
+                  ],
                 ),
               );
+            }).toList(),
+            const SizedBox(height: 8),
+            addaddresas(context, _addAddressField),
+          ],
+        ),
+      ),
+                
+          const SizedBox(height: 5),
+          CustomElevatedButton(
+            textdata: "Save",
+            onPressed: _saveProfile,
+          ),
+         ],
+         ),
+          ),
+        );
             }
             return const Center(child: Text("No profile data available."));
           },
